c31a13bed0f423e66757ecb36c5d1dd0066791c4
Josue <toler.gm@gmail.com>
2026-08-26 12:21:12 -0500

    build correcto docker y jenkins

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,29 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  define: {
-    'process.env': {}
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(import.meta.dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  console.log(`🔧 Modo de build: ${mode}`);
+  const env = loadEnv(mode, process.cwd(), '');
+  console.log(`🔧 VITE_API: ${env.VITE_API}`);
+  return {
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(import.meta.dirname, "./src"),
+      },
     },
-  },
+    define: {
+      'process.env.VITE_API': JSON.stringify(env.VITE_API),
+      'process.env.VITE_API_NESTLE': JSON.stringify(env.VITE_API_NESTLE),
+      'process.env.VITE_API_MEGALAB': JSON.stringify(env.VITE_API_MEGALAB),
+      'process.env.VITE_API_ALMACENES': JSON.stringify(env.VITE_API_ALMACENES),
+      'process.env.VITE_API_CONDOMINIOS': JSON.stringify(env.VITE_API_CONDOMINIOS),
+      'process.env.VITE_RECAPTCHA_TOKEN': JSON.stringify(env.VITE_RECAPTCHA_TOKEN),
+      'process.env.VITE_MAPBOX_TOKEN': JSON.stringify(env.VITE_MAPBOX_TOKEN),
+      'process.env.VITE_POWERBI_URL': JSON.stringify(env.VITE_POWERBI_URL),
+      'process.env.VITE_GOOGLE_CLIENT_ID': JSON.stringify(env.VITE_GOOGLE_CLIENT_ID),
+    }
+  };
 });
